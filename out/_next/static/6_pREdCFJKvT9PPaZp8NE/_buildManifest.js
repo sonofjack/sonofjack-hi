e778795c4066fe1463f8d36db0fbf2c4f2d12891
Jackson Rowe <jackson@autopilothq.com>
2022-02-09 09:36:49 +1100

    build: deploy next.js to gh-pages

--- out/_next/static/6_pREdCFJKvT9PPaZp8NE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5292d4877af909a7.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],"/about":["static/chunks/pages/about-7dd8f43b0bd8b22b.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
