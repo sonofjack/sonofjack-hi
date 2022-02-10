879b0d9054be9c1c81840edf8e444b50780cf3b6
Jackson Rowe <jackson@autopilothq.com>
2022-02-11 08:24:27 +1100

    fix: updated font paths

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,14 +8,14 @@ export default class Document extends NextDocument {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="preload" href="/fonts/Roboto-Black.woff" as="font" type="font/woff" crossOrigin="anonymous" />
-					<link rel="preload" href="/fonts/Roboto-Black.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-					<link rel="preload" href="/fonts/Roboto-Bold.woff" as="font" type="font/woff" crossOrigin="anonymous" />
-					<link rel="preload" href="/fonts/Roboto-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-					<link rel="preload" href="/fonts/Roboto-Medium.woff" as="font" type="font/woff" crossOrigin="anonymous" />
-					<link rel="preload" href="/fonts/Roboto-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-					<link rel="preload" href="/fonts/Roboto-Regular.woff" as="font" type="font/woff" crossOrigin="anonymous" />
-					<link rel="preload" href="/fonts/Roboto-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Black.woff" as="font" type="font/woff" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Black.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Bold.woff" as="font" type="font/woff" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Medium.woff" as="font" type="font/woff" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Regular.woff" as="font" type="font/woff" crossOrigin="anonymous" />
+					<link rel="preload" href="./fonts/Roboto-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
 					<style
 						dangerouslySetInnerHTML={{
 							__html: `
@@ -23,27 +23,27 @@ export default class Document extends NextDocument {
   font-family: 'Roboto';
   font-weight: 900;
   font-display: swap;
-  src: url(/fonts/Roboto-Black.woff2) format('woff2'), url(/fonts/Roboto-Black.woff) format('woff');
+  src: url(./fonts/Roboto-Black.woff2) format('woff2'), url(./fonts/Roboto-Black.woff) format('woff');
 }
 @font-face {
   font-family: 'Roboto';
   font-weight: 700;
   font-display: swap;
-  src: url(/fonts/Roboto-Bold.woff2) format('woff2'), url(/fonts/Roboto-Bold.woff) format('woff');
+  src: url(./fonts/Roboto-Bold.woff2) format('woff2'), url(./fonts/Roboto-Bold.woff) format('woff');
 }
 @font-face {
   font-family: 'Roboto';
   font-weight: 500;
   font-style: normal;
   font-display: swap;
-  src: url('/fonts/Roboto-Medium.woff2') format('woff2'), url('/fonts/Roboto-Medium.woff') format('woff');
+  src: url('./fonts/Roboto-Medium.woff2') format('woff2'), url('./fonts/Roboto-Medium.woff') format('woff');
 }
 @font-face {
   font-family: 'Roboto';
   font-weight: 400;
   font-style: normal;
   font-display: swap;
-  src: url('/fonts/Roboto-Regular.woff2') format('woff2'), url('/fonts/Roboto-Regular.woff') format('woff');
+  src: url('./fonts/Roboto-Regular.woff2') format('woff2'), url('./fonts/Roboto-Regular.woff') format('woff');
 }
 `,
 						}}
