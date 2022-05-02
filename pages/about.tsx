13b206b93139737536518135a1158d67a949f9de
Jackson Rowe <jackson@autopilothq.com>
2022-05-02 14:52:49 +1000

    my random commit
    another one
    
    my fav
    
    another

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,8 +3,12 @@ import Link from 'next/link'
 export default function About() {
 	return (
 		<div>
+			<div>NEWW another new boy</div>
 			<div>About us</div>
 			<div>About us</div>
+			<div>About us</div>
+			<div>wow</div>
+			<div>wow</div>
 			<div>
 				Back to{' '}
 				<Link href="/" as={process.env.BACKEND_URL + '/'}>
