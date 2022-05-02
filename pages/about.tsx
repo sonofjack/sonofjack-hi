7839f97bd707d984cc85b5cbbda71606cd055330
Jackson Rowe <jackson@autopilothq.com>
2022-05-02 14:48:41 +1000

    fix: remove husky

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ export default function About() {
 	return (
 		<div>
 			<div>About us</div>
+			<div>About us</div>
 			<div>
 				Back to{' '}
 				<Link href="/" as={process.env.BACKEND_URL + '/'}>
